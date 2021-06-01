b3bdab0cf68d55a46e068d295551c87ba046800a
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-06-01 10:58:17 +0200

    WindowResize now make sure that the object aren't scaled with it.

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -13,6 +13,8 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::Resized)
+                window.setView(sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
         }
 
         window.clear();
